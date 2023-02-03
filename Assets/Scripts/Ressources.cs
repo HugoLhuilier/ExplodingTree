2bbebd4656de507b9274f21bb7b678fdd72311f0
toto titi <toto@tata.titi>
2023-02-04 00:46:13 +0100

    test

---
+++ Assets/Scripts/Ressources.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ressources : MonoBehaviour
+{
+
+    public int[] resources = new int[4];
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(resources[1]);
+    }
+}
