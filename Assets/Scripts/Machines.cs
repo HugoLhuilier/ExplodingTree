307b58cafdbe2b9d785b619af3acd11c9e94b51d
toto titi <toto@tata.titi>
2023-02-03 22:25:27 +0100

    script machine

--- Assets/Scripts/Machines.cs
+++ Assets/Scripts/Machines.cs
@@ -1,19 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Machines : MonoBehaviour
 {
-    
+    float compteur = 0;
+    [SerializeField] int profondeur;
+    [SerializeField] int generate;
+    [SerializeField] int resources;
+    [SerializeField] GameObject player;
+    [SerializeField] int type;
+    Boolean collide;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        compteur += Time.deltaTime;
+        if (compteur >= generate)
+        {
+            compteur -= generate;
+            resources += profondeur;
+        }
+        if (Input.GetKeyDown(KeyCode.E) && collide)
+        {
+            player.GetComponent<playerMovement>().resources[type] += resources;
+            resources = 0;
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            collide = true;
+        }
     }
 }
--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+
+    [SerializeField] public int[] resources = new int[4];
     // Start is called before the first frame update
     void Start()
     {
