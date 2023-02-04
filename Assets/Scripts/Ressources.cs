cf44cefc8163a5605d2cd1e6419504d8331a5fb5
Adrisch <adrien.schroedel91@gmail.com>
2023-02-04 21:36:56 +0100

    GetHit corrigé

--- Assets/Scripts/Ressources.cs
+++ Assets/Scripts/Ressources.cs
@@ -19,26 +19,21 @@ public class Ressources : MonoBehaviour
     }
 
 
+    private void Update()
+    {
+        
+    }
+
+
+
     public void GetHit(float damage)
     {
-<<<<<<< HEAD
-
-        
-        
-
-=======
->>>>>>> e4d0918f1cfbc86577ad6b76664c2f6593874531
         healthPoints -= damage;
         if(healthPoints <= 0)
         {
             // Load Gameover screen
         }
-<<<<<<< HEAD
-
-=======
->>>>>>> e4d0918f1cfbc86577ad6b76664c2f6593874531
     }
-
 
 
     public int getCoins()
