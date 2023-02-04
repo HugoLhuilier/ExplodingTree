40d9a03747b184b926893237ab61bd774be7de63
ValT <v.lantigny@gmail.com>
2023-02-04 23:41:48 +0100

    PlaceHolder des armes

--- Assets/Scripts/UIShopClose.cs
+++ Assets/Scripts/UIShopClose.cs
@@ -2,25 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIClose : MonoBehaviour
+public class UIShopClose : MonoBehaviour
 {
-    public GameObject motherWeapon;
+    public GameObject motherShop;
     [SerializeField] private GameObject mainUI;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) )
+        if (Input.GetMouseButtonDown(0))
         {
             Vector3 mousePosition = Input.mousePosition;
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            if (Vector2.Distance(worldPosition, motherWeapon.transform.position) > 2) {
+
+            if (Vector2.Distance(worldPosition, motherShop.transform.position) > 2)
+            {
                 Destroy(mainUI);
             }
         }
