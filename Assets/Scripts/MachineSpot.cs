dafabef3ec009844a34664dc275f45e58f7a1750
toto titi <toto@tata.titi>
2023-02-04 10:04:53 +0100

    test

--- Assets/Scripts/MachineSpot.cs
+++ Assets/Scripts/MachineSpot.cs
@@ -21,7 +21,7 @@ public class MachineSpot : MonoBehaviour
 
     public void buy(Ressources player)
     {
-        if (player.getCoins() >= cost)
+        if (player.getCoins(1) >= cost)
         {
             //FAIRE BAISSER THUNES DU JOUEUR
             Instantiate(machine);
